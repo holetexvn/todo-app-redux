48342644dbfa2e2799665348760de5c5a11c1cea
holetexvn <holetex@outlook.com>
2021-12-07 16:24:10 +0700

    add checked state for each todo item

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,4 +1,5 @@
-import { Row, Tag, Checkbox, Input } from 'antd';
+import { Row, Tag, Checkbox } from 'antd';
+import { useState } from 'react';
 
 const priorityColorMapping = {
   High: 'red',
@@ -7,9 +8,23 @@ const priorityColorMapping = {
 };
 
 export default function Todo({ name, prioriry }) {
+  const [checked, setChecked] = useState(false);
+
+  const toggleCheckbox = () => {
+    setChecked(!checked);
+  };
+
   return (
-    <Row justify='space-between' style={{ marginBottom: 3 }}>
-      <Checkbox>{name}</Checkbox>
+    <Row
+      justify='space-between'
+      style={{
+        marginBottom: 3,
+        ...(checked ? { opacity: 0.5, textDecoration: 'line-through' } : {}),
+      }}
+    >
+      <Checkbox checked={checked} onChange={toggleCheckbox}>
+        {name}
+      </Checkbox>
       <Tag color={priorityColorMapping[prioriry]} style={{ margin: 0 }}>
         {prioriry}
       </Tag>
